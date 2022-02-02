db41337fe47d62fb09c679fe2fd82d8d90cf3789
Alex <alex.fritzacus@gmail.com>
2022-02-02 15:57:04 -0600

    Add files via upload

---
+++ ValueConverterDemo/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ValueConverterDemo.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SolidColorBrush scb = null;
+
+            if(targetType == typeof(Brush))
+            {
+                bool boolVal = (bool)value;
+
+                //Pro Tip: You can use a ternary here to set the brush
+                scb = boolVal ? Brushes.Yellow : Brushes.Purple;
+            }
+
+            return scb;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
