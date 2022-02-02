f3b8f07ee7a4db58ce38527107479b7e59a0d9e6
Alex <alex.fritzacus@gmail.com>
2022-02-02 15:49:50 -0600

    Add files via upload

---
+++ ItemsControlsDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItemsControlsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //generate a collection
+            int[] nums = Enumerable.Range(24, 10).ToArray();   //just built an array from 24 to 33  
+
+
+
+            //set the items source of the items control to the collection
+            ComboBox.ItemsSource = nums;
+
+
+        }
+    }
+}
---
+++ MenusAndCommandsDemo/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenusAndCommandsDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //This was our first Close handler that simply prints a message.
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Why you tryin' to close me, bro?!");
+        }
+
+        //This is our current Close handler that prints a message AND closes the application
+        private void OtherClose_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ok, I'll really close this time.");
+            Application.Current.Shutdown();
+        }
+
+        //This is the Save handler that prints a message
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Saving... Saved!");
+        }
+    }
+}
